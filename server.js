679938862941f2f2a4b1fc432892114e706bf940
yub <yamadayub@gmail.com>
2025-05-25 13:35:29 +0900

    Fix housing app routing in server.js

--- server.js
+++ server.js
@@ -23,6 +23,19 @@ app.use('/sugoroku', express.static(join(__dirname, 'dist/sugoroku')));
 // marketplaceアプリのためのStatic Files
 app.use(express.static(join(__dirname, 'dist/marketplace')));
 
+// housingのSPA処理 - /housing/で始まるすべてのルートを処理
+app.get('/housing/*', (req, res) => {
+  console.log(`Serving housing for path: ${req.path}`);
+  res.sendFile(join(__dirname, 'dist/housing/index.html'));
+});
+
+// sugorokuのSPA処理 - /sugoroku/で始まるすべてのルートを処理
+app.get('/sugoroku/*', (req, res) => {
+  console.log(`Serving sugoroku for path: ${req.path}`);
+  res.sendFile(join(__dirname, 'dist/sugoroku/index.html'));
+});
+
+// リダイレクト処理
 app.get('/housing', (req, res) => {
   res.redirect(301, '/housing/');
 });
@@ -31,24 +44,11 @@ app.get('/sugoroku', (req, res) => {
   res.redirect(301, '/sugoroku/');
 });
 
-// housingのSPA処理 - 明示的に/housingと/housing/を処理
-app.get(['/housing', '/housing/*'], (req, res) => {
-  console.log(`Serving housing for path: ${req.path}`);
-  res.sendFile(join(__dirname, 'dist/housing/index.html'));
-});
-
-// sugorokuのSPA処理 - 明示的に/sugorokuと/sugoroku/を処理
-app.get(['/sugoroku', '/sugoroku/*'], (req, res) => {
-  console.log(`Serving sugoroku for path: ${req.path}`);
-  res.sendFile(join(__dirname, 'dist/sugoroku/index.html'));
-});
-
 // marketplaceのSPA処理 (その他のルート)
 app.get('/*', (req, res) => {
   console.log(`Serving marketplace for path: ${req.path}`);
   res.sendFile(join(__dirname, 'dist/marketplace/index.html'));
 });
-
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
