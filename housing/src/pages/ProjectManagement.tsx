0d0fc1dac8c96461afbef6b54ddb824126748399
yub <yamadayub@gmail.com>
2025-06-01 08:03:18 +0900

    builder portal

--- housing/src/pages/ProjectManagement.tsx
+++ housing/src/pages/ProjectManagement.tsx
@@ -659,7 +659,7 @@ const ProjectManagement: React.FC = () => {
           <div className="flex items-center justify-between h-16">
             <div className="flex items-center space-x-4">
               <button
-                onClick={() => navigate('/contractor-portal')}
+                onClick={() => navigate('/builder-portal')}
                 className="inline-flex items-center text-gray-600 hover:text-gray-900 transition-colors"
               >
                 <ArrowLeft className="w-5 h-5 mr-1" />
